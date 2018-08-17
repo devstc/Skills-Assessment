49155253426adfa07f2214c5578bc3ead2d2cc2c
Sean Cramer <stc_1138@yahoo.com>
2018-08-17 10:27:38 -0500

    commit again

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   // write your scripts here
-<<<<<<< HEAD
 
   function pickPic(max) {
 		var num = Math.floor(Math.random() * Math.floor(max)) + 1;
@@ -9,6 +8,4 @@ $(document).ready(function() {
   
   pickPic(3);
 
-=======
->>>>>>> 049d816bbf903ae1e0ea4d6b792ff1360f28d959
 });
